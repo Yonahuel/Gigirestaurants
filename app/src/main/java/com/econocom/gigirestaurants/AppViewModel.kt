8898f3140444c8c53bd36d6e968694736592bf4c
Yonahuel <yonahuel2006@hotmail.com>
2024-02-13 19:28:44 -0300

    Estructura básica creada

---
+++ app/src/main/java/com/econocom/gigirestaurants/AppViewModel.kt
@@ -0,0 +1,12 @@
+package com.econocom.gigirestaurants
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class AppViewModel(application: Application): AndroidViewModel(application) {
+
+
+    init {
+
+    }
+}
---
+++ app/src/main/java/com/econocom/gigirestaurants/model/network/KtorEngine.kt
@@ -0,0 +1,4 @@
+package com.econocom.gigirestaurants.model.network
+
+class KtorEngine {
+}
